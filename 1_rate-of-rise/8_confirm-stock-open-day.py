20e19aa79601ca870d21bc8e28d6ef7c02e29112
박경태 <gyeongtae.park@catenoid.net>
2022-05-25 20:36:43 +0900

    주식 개장일 관련 설명 추가

--- 1_rate-of-rise/8_confirm-stock-open-day.py
+++ 1_rate-of-rise/8_confirm-stock-open-day.py
@@ -1,11 +1,27 @@
 import datetime
 import exchange_calendars as ecals 
 
+# 우리나라 국가 코드
 XKRX = ecals.get_calendar("XKRX")
 
-print(XKRX.is_session("2022-05-04"))
-print(XKRX.is_session("2022-05-05"))
-print(XKRX.is_session("2022-05-06"))
+# 해당 날짜가 주식을 하는 날인가? (True / False)
+# print(XKRX.is_session("2022-05-04"))
+# print(XKRX.is_session("2022-05-05"))
+# print(XKRX.is_session("2022-05-06"))
 
-print(XKRX.is_session(datetime.date.today()))
-print(XKRX.next_open(datetime.date.today()).strftime("%Y-%m-%d"))
+# 오늘이 주식을 하는 날인가? (True / False)
+# print(XKRX.is_session(datetime.date.today()))
+
+# 오늘을 기준으로 다음 주식 개장일은 언제인가? (yyyy-mm-dd)
+# print(XKRX.next_open(datetime.date.today()).strftime("%Y-%m-%d"))
+
+
+# print(XKRX.next_open(datetime.date.today())(XKRX.next_open(datetime.date.today()).strftime("%Y%m%d")))
+
+aaa = XKRX.next_open(datetime.date.today()).strftime("%Y-%m-%d")
+
+format = '%Y-%m-%d'
+dt_datetime = datetime.datetime.strptime(aaa, format)
+
+print(dt_datetime)
+print(type(dt_datetime))
