97311b7be4d38f91628286662ed1f2fc674fcfa6
박경태 <gyeongtae.park@catenoid.net>
2022-05-25 20:48:24 +0900

    특정 날짜를 기준으로 다음 개장일과 다다음 개장일 출력

--- 1_rate-of-rise/8_confirm-stock-open-day.py
+++ 1_rate-of-rise/8_confirm-stock-open-day.py
@@ -4,24 +4,31 @@ import exchange_calendars as ecals
 # 우리나라 국가 코드
 XKRX = ecals.get_calendar("XKRX")
 
-# 해당 날짜가 주식을 하는 날인가? (True / False)
+# 해당 날짜가 주식을 하는 날인가?
+# Result : True / False
 # print(XKRX.is_session("2022-05-04"))
 # print(XKRX.is_session("2022-05-05"))
 # print(XKRX.is_session("2022-05-06"))
 
-# 오늘이 주식을 하는 날인가? (True / False)
+# 오늘이 주식을 하는 날인가? 
+# Result : True / False
 # print(XKRX.is_session(datetime.date.today()))
 
-# 오늘을 기준으로 다음 주식 개장일은 언제인가? (yyyy-mm-dd)
+# 오늘을 기준으로 다음 주식 개장일은 언제인가?
+# Result : yyyy-mm-dd
 # print(XKRX.next_open(datetime.date.today()).strftime("%Y-%m-%d"))
 
 
-# print(XKRX.next_open(datetime.date.today())(XKRX.next_open(datetime.date.today()).strftime("%Y%m%d")))
+# 특정 날짜를 기준으로 다음 개장일과 다다음 개장일은 언제인가?
 
-aaa = XKRX.next_open(datetime.date.today()).strftime("%Y-%m-%d")
-
+date = '2022-05-04'
 format = '%Y-%m-%d'
-dt_datetime = datetime.datetime.strptime(aaa, format)
 
-print(dt_datetime)
-print(type(dt_datetime))
+dt_date = datetime.datetime.strptime(date, format).strftime(format)
+st_date = XKRX.next_open(dt_date).strftime(format)
+dt_next_date = datetime.datetime.strptime(st_date, format)
+st_next_date = XKRX.next_open(dt_next_date).strftime(format)
+
+print(dt_date)
+print(st_date)
+print(st_next_date)
