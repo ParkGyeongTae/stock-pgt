40985b47a9bb8b86839dc8ed334f0fb916b9ca57
박경태 <gyeongtae.park@catenoid.net>
2022-05-25 22:29:27 +0900

    고가팔기 추가

--- 1_rate-of-rise/1_compare_시종등락률기준_고가팔기.py
+++ 1_rate-of-rise/1_compare_시종등락률기준_고가팔기.py
@@ -30,8 +30,8 @@ evaluation_date = st_next_date
 
 for stock_file in stock_file_list:
     df = pd.read_excel(f'./stock-data/2022-05/{stock_file}')
-    df = df[['종목명', '종가', '등락률']]
-    df.columns = ['종목명', f'종가_{stock_file[10:14]}', f'등락률_{stock_file[10:14]}']
+    df = df[['종목명', '종가', '고가', '등락률']]
+    df.columns = ['종목명', f'종가_{stock_file[10:14]}', f'고가_{stock_file[10:14]}', f'등락률_{stock_file[10:14]}']
     globals()[f'{stock_file.split(".")[0]}'] = df
 
 merge_df = pd.merge(globals()[f'kospi_{start_date}'], 
@@ -50,9 +50,9 @@ merge_df = merge_df.sort_values(by = [f'등락률_{end_date[4:]}'], ascending = 
 
 # print(tabulate(merge_df.head(10), headers = 'keys', tablefmt = 'pretty'))
 
-# print('<<< Summary >>>')
-print(f'{end_date} 상위 10개 :', merge_df[f'종가_{end_date[4:]}'].head(10).sum()), '원'
-print(f'{evaluation_date} 상위 10개 :', merge_df[f'종가_{evaluation_date[4:]}'].head(10).sum()), '원'
-print(f'{end_date} - {evaluation_date} :', merge_df[f'종가_{evaluation_date[4:]}'].head(10).sum() - merge_df[f'종가_{end_date[4:]}'].head(10).sum(), '원', 
-                                        round((merge_df[f'종가_{evaluation_date[4:]}'].head(10).sum() - merge_df[f'종가_{end_date[4:]}'].head(10).sum()) 
+print('<<< Summary >>>')
+print(f'{end_date} 상위 10개 종가합 :', merge_df[f'종가_{end_date[4:]}'].head(10).sum()), '원'
+print(f'{evaluation_date} 상위 10개 고가합 :', merge_df[f'고가_{evaluation_date[4:]}'].head(10).sum()), '원'
+print(f'{end_date} - {evaluation_date} :', merge_df[f'고가_{evaluation_date[4:]}'].head(10).sum() - merge_df[f'종가_{end_date[4:]}'].head(10).sum(), '원', 
+                                        round((merge_df[f'고가_{evaluation_date[4:]}'].head(10).sum() - merge_df[f'종가_{end_date[4:]}'].head(10).sum()) 
                                         / merge_df[f'종가_{end_date[4:]}'].head(10).sum() * 100, 2), '%')
